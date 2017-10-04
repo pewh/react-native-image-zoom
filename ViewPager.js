172e4ca7af27886b299087737a4be7d5f3366441
Pewh Gosh <pwr.gosh@gmail.com>
2017-10-04 13:39:27 +0700

    Update ViewPager.js

--- ViewPager.js
+++ ViewPager.js
@@ -1,5 +1,6 @@
+import PropTypes from 'prop-types';
 
-import React,{Component,PropTypes} from 'react';
+import React,{PropTypes} from 'react';
 import {
   requireNativeComponent,
   ViewPagerAndroid,
